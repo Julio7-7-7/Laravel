a25ca34a3d1ea129f44743e79397123e536c7e17
Julio7-7-7 <julio.toledo2030@gmail.com>
2025-09-04 07:14:57 -0600

    no sè

--- tiendaLaravel/resources/views/welcome.blade.php
+++ tiendaLaravel/resources/views/welcome.blade.php
@@ -1,15 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hola waos</h1>
-</body>
-</html>
-=======
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
