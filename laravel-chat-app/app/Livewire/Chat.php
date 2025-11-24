94a36c778b06db87b1acd17269587789608db70b
Julio7-7-7 <julio.toledo2030@gmail.com>
2025-11-24 13:29:26 -0400

    real time

--- laravel-chat-app/app/Livewire/Chat.php
+++ laravel-chat-app/app/Livewire/Chat.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire;
 
+use App\Events\MessageSent;
 use App\Models\ChatMessage;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Symfony\Component\Mime\Message;
 
 class Chat extends Component
 {
@@ -14,12 +16,14 @@ class Chat extends Component
     public $selectedUser;
     public $newMessage;
     public $messages;
+    public $loginID;
 
     public function mount()
     {
         $this->users = User::whereNot("id", Auth::id())->latest()->get();
         $this->selectedUser = $this->users->first();
         $this->loadMessages();
+        $this->loginID = Auth::id();
     }
 
     function selectUser($id)
@@ -41,6 +45,35 @@ class Chat extends Component
         $this->messages->push($message);
 
         $this->newMessage = '';
+
+        broadcast(new MessageSent($message));
+    }
+
+    public function getListeners()
+    {
+        return [
+            "echo-private:chat.{$this->loginID},MessageSent" => 'newChatMessageNotification',
+        ];
+    }
+
+    public function newChatMessageNotification($event)
+    {
+        if (!$this->selectedUser) {
+            return;
+        }
+
+        $senderId = $event['sender_id'];
+        $receiverId = $event['receiver_id'];
+
+        // Solo agregar si es conversación actual
+        if ($senderId == $this->selectedUser->id || $receiverId == $this->selectedUser->id) {
+            // ¡Importante! Buscar el mensaje como modelo real
+            $message = ChatMessage::find($event['id']);
+
+            if ($message) {
+                $this->messages->prepend($message);
+            }
+        }
     }
 
     public function loadMessages()
